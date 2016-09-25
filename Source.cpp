0e7c6023a81418c579049b499fa81ec6e32fcd26
Niloofaro <nofg8@mail.umkc.edu>
2016-09-25 17:39:13 -0500

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -1,6 +1,22 @@
 /*
 Name: Hamza Chaudhry, Fatima Mohamed, Niloofar Oliaei
 Description:
+ #include <string>       // std::string
+#include <iostream>     // std::cout
+#include <sstream> 
+
+using namespace std;
+
+void main() {
+	string poly = "5X-5X^-2+10-5X+X^2";
+	istringstream tokens(poly);
+	char next_char;
+	while (tokens >> next_char) {
+		cout << next_char << endl;
+	}
+
+
+}
 
 */
 #include <iostream>
