a83788f5a79aaa521b7d77de85180176fa9c4686
hactyd <chumza64@gmail.com>
2016-09-25 13:38:35 -0500

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -7,6 +7,7 @@ Description:
 #include <string>
 #include <algorithm>
 #include <vector>
+#include <stdlib.h>
 using namespace std;
 #include "Term.h"
 
@@ -27,40 +28,63 @@ int main() {
 		if (tempInd != string::npos) { // if there is an exponent
 			expon = polyString.substr(tempInd+1); // grab the exponent value
 			coeff = polyString.substr(0, tempInd);
-			if (coeff.find("X") != string::npos) {	// if coeff does have X in it
+			if (coeff.find("X") != string::npos || NULL) {	// if coeff does have X in it
 				coeff = coeff.substr(0,coeff.length()-1); // cleave off the X to get only coefficient int
 			}
-			cout << "The exponent is: " << expon;
-			cout << "The coefficient is: " << coeff;
-		}
-	}
-	
-
-	/*getline(cin, polyString);
-	cout << "The polynomial got stored as " << polyString;
-
-	int poly1Len = polyString.length() -1;
-	int index = 0;
-	int terms = 1;
-
-	while (index <= poly1Len) {
-		// Check to to find +, means new term is defined
-		if (polyString[index] == '+') {
-			terms++;
-		}
-		// Multiple exceptions for -
-		if (polyString[index] == '-') {
-			if (isdigit(polyString[index - 1])) {
-				terms++;
-			}
-			if (polyString[index - 1] == 'X' || 'x') {
-				terms++;
+			else { // otherwise it has no X but it does have an exponent
+				coeff = polyString.substr(0, tempInd);
 			}
 		}
-		index++;
+		else { // the term has no exponent
+			expon = "1";
+			if (polyString.find("X") || polyString.find("x") != string::npos) { // if value does have X in it
+				coeff = coeff.substr(0, coeff.length() - 1); // cleave off the X to get only coefficient int
+			}
+			else {	// there is no X, only the value.
+				coeff = polyString;
+			}
+		}
+		int coeffInt = stoi(coeff);
+		int exponInt = stoi(expon);
+
+		Term monomial = Term(coeffInt, exponInt);
+		list.push_back(monomial);
+	}
+	if (polyString == "S" || "s") {
+		cout << "You may now enter your second polynomial term-by-term, pressing enter to add a new term. Type 'e' to evaluate your polynomials." << endl;
+		while (polyString != "E" || "e") {
+			getline(cin, polyString);	// retrieve the next term
+			int tempInd = polyString.find("^");
+			if (tempInd != string::npos) { // if there is an exponent
+				expon = polyString.substr(tempInd + 1); // grab the exponent value
+				coeff = polyString.substr(0, tempInd);
+				if (coeff.find("X") != string::npos || NULL) {	// if coeff does have X in it
+					coeff = coeff.substr(0, coeff.length() - 1); // cleave off the X to get only coefficient int
+				}
+				else { // otherwise it has no X but it does have an exponent
+					coeff = polyString.substr(0, tempInd);
+				}
+			}
+			else { // the term has no exponent
+				expon = "1";
+				if (polyString.find("X") || polyString.find("x") != string::npos) { // if value does have X in it
+					coeff = coeff.substr(0, coeff.length() - 1); // cleave off the X to get only coefficient int
+				}
+				else {	// there is no X, only the value.
+					coeff = polyString;
+				}
+			}
+			int coeffInt = stoi(coeff);
+			int exponInt = stoi(expon);
+
+			Term monomial = Term(coeffInt, exponInt);
+			list.push_back(monomial);
+		}
+	}
+	for (int i = 0; i < list.size(); i++) {
+		//cout << "The term is: " << 
 	}
 
-	cout << "There are " << terms << " terms.";*/
 	int age;
 	cin >> age;
 	
